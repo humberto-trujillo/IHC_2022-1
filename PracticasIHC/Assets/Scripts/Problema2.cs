dd66e0091a8aa428feabb4e90088d4ce3aa655b5
Humberto Trujillo <htp.tito@gmail.com>
2022-02-18 19:08:51 -0800

    terminando problema 2

--- PracticasIHC/Assets/Scripts/Problema2.cs
+++ PracticasIHC/Assets/Scripts/Problema2.cs
@@ -15,4 +15,13 @@ public class Problema2 : MonoBehaviour
     {
         
     }
+
+    private bool CompareTestResult(int[] result, int[] expectedResult)
+    {
+        for (int i = 0; i < result.Length; i++)
+        {
+            if (result[i] != expectedResult[i]) return false;
+        }
+        return true;
+    }
 }
